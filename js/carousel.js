c93fda878ae844cf57cde552a64631d053e55e69
Nikhil Gupta <22201788+nikhilro@users.noreply.github.com>
2019-01-07 18:26:32 -0500

    added carousel (#9)
    * added carousel
    
    * fit to mocks and moved carousel scss to main.scss
    
    * made requested changes by @derrekchow
    
    ## stretched first image bug still exists

---
+++ js/carousel.js
@@ -0,0 +1,16 @@
+$('.carousel .carousel-item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    for (var i=0;i<1;i++) {
+        next=next.next();
+        if (!next.length) {
+        	next = $(this).siblings(':first');
+      	}
+        
+        next.children(':first-child').clone().appendTo($(this));
+      }
+});
