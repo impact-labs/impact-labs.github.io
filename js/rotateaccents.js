2c77c799a767e316d817790472c1c6153bc2f68c
derrekchow <derrekdc@gmail.com>
2019-03-14 16:15:45 -0400

    quick fix

--- js/rotateaccents.js
+++ js/rotateaccents.js
@@ -1,5 +1,5 @@
 AOS.init({
-	offset: 400, // offset (in px) from the original trigger point
+	offset: 200, // offset (in px) from the original trigger point
 	duration: 800, // values from 0 to 3000, with step 50ms
 	easing: 'ease-out', // default easing for AOS animations
 	once: false, // whether animation should happen only once - while scrolling down
